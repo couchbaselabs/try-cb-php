40e603039c04a7b5ea5da7e4c84861ff35db7c61
Danzibob <Danzibob@gmail.com>
2019-08-15 13:27:11 +0100

    Update libraries; Manually connect to CB-Server

--- app/Http/Controllers/CouchbaseController.php
+++ app/Http/Controllers/CouchbaseController.php
@@ -15,6 +15,15 @@ class CouchbaseController extends Controller
      */
     public function __construct()
     {
-        $this->db = DB::connection('couchbase')->openBucket('travel-sample');
+        // Establish username and password for bucket-access
+        $authenticator = new \Couchbase\PasswordAuthenticator();
+        $authenticator->username('Administrator')->password('password');
+
+        // Connect to Couchbase Server
+        $cluster = new \CouchbaseCluster("couchbase://127.0.0.1");
+        // Authenticate, then open bucket
+        $cluster->authenticate($authenticator);
+
+        $this->db = $cluster->openBucket('travel-sample');
     }
 }
